93db4247f9102d2033db9858b16db51b775d5301
Johannes Herrmann <johannes@jhlabs.io>
2021-03-19 08:20:50 +0100

    adds top bar to game

--- src/packages/memory/ui/memory-game/memory-game.tsx
+++ src/packages/memory/ui/memory-game/memory-game.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Box, Center } from "@chakra-ui/layout";
+import { Box, Center, Flex, Heading } from "@chakra-ui/layout";
 import { Deck } from "../deck";
 import { ContentType, generateGame, ImageContent, Level } from "../../domain";
 import { PrototypGangMock } from "../../mock-data/prototyp-gang";
 import { StartGame } from "../start-game";
+import Icon from "@chakra-ui/icon";
+import { IoClose } from "react-icons/io5";
 
 const getCardsFromLevel = generateGame(PrototypGangMock);
 
@@ -20,12 +22,25 @@ export const MemoryGame = () => {
   };
 
   return (
-    <Center bgColor="gray.100" minHeight="100vh">
-      {level ? (
-        <Deck cards={getCardsFromLevel(level)} cover={prototypCover} />
-      ) : (
-        <StartGame onSelectLevel={onSetLevel} />
-      )}
-    </Center>
+    <Box bgColor="gray.100">
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        paddingY={8}
+        paddingX={16}
+      >
+        <Heading size="md" marginLeft="auto">
+          Your score: 100
+        </Heading>
+        <Icon as={IoClose} marginLeft="auto" width={12} height={12} />
+      </Flex>
+      <Center minHeight="100vh">
+        {level ? (
+          <Deck cards={getCardsFromLevel(level)} cover={prototypCover} />
+        ) : (
+          <StartGame onSelectLevel={onSetLevel} />
+        )}
+      </Center>
+    </Box>
   );
 };
