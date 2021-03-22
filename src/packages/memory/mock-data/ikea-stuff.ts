457bcef4293d149f4b720d6b25589c86d56c094b
Johannes Herrmann <johannes@jhlabs.io>
2021-03-22 16:38:30 +0100

    adds ikea data

--- src/packages/memory/mock-data/ikea-stuff.ts
+++ src/packages/memory/mock-data/ikea-stuff.ts
@@ -1,3 +1,124 @@
-import { ContentList } from "../domain";
+import { ContentList, ContentType } from "../domain";
 
-export const IkeaStuff: ContentList = [];
+export const IkeaStuff: ContentList = [
+  [
+    { content: "Bolmen", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/bolmen-toalettborste-hallare-svart__0711691_pe728384_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Gubbröra", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/gubbroera-bakpensel-vit-svart__0713022_pe729201_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Bördig", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/boerdig-skohorn-bambu__0923122_pe788297_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Klabb", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/klabb-bordslampa-off-white-foernicklad__0676723_pe718793_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Gurli", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/gurli-kuddfodral-gyllengul__0544894_pe655202_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Svalnäs", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/svalnaes-hyllplan-bambu__0640705_pe700005_s5.jpg?f=g",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Svartsjön", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/svartsjoen-toalettrullehallare-svart__0711713_pe728406_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Strandmon", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/strandmon-oeronlappsfatoelj-nordvalla-roed__0690287_pe723209_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Skynke", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/skynke-baerkasse-gul-vit__0778792_pe759148_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Fejka", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/fejka-konstgjord-krukvaext-inom-utomhus-palm__0797076_pe766755_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Kalas", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/kalas-tallrik-flerfaergad__0652060_pe707256_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Klughet", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/klokhet-ljus-utan-doft-lila__0918921_pe786513_s5.jpg?f=g",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Lack", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/lack-tv-baenk-svartbrun__0644449_pe702700_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Lagt", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/lagt-strykbraedsoeverdrag-gul__0490231_pe624342_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+  [
+    { content: "Fluns", type: ContentType.Text },
+    {
+      content:
+        "https://www.ikea.com/se/sv/images/products/fluns-tidskriftssamlare-vit__0398648_pe563794_s5.jpg?f=xxxl",
+      type: ContentType.Image,
+    },
+  ],
+];
